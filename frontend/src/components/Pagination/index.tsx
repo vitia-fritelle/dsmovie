556ce317e15be4bbe94c2f17b80aede7d5743e76
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2022-01-16 17:37:25 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,37 +1,24 @@
 import {ReactComponent as Arrow} from 'assets/img/arrow.svg';
-import { useState } from 'react';
+import { MoviePage } from 'types/movie';
 import './styles.css';
 
-const Pagination = () => {
+type Props = {
+    page: MoviePage;
+    onChange: Function;
+};
 
-    const [isArrowLeftDisabled, setArrowLeft] = useState(true);
-    const [isArrowRightDisabled, setArrowRight] = useState(false);
-
-    const disableArrowLeft = () => {
-        setArrowLeft(false);
-    }
-
-    const enableArrowLeft = () => {
-        setArrowLeft(true);
-    }
-
-    const disableArrowRight = () => {
-        setArrowRight(false);
-    }
-
-    const enableArrowRight = () => {
-        setArrowRight(true);
-    }
-
+const Pagination = ({page, onChange} : Props) => {
 
     return (
         <div className="dsmovie-pagination-container">
             <div className="dsmovie-pagination-box">
-                <button className="dsmovie-pagination-button" disabled={isArrowLeftDisabled} >
+                <button className="dsmovie-pagination-button" 
+                 disabled={page.first} onClick={() => onChange(page.number-1)} >
                     <Arrow />
                 </button>
-                <p>{`${1} de ${3}`}</p>
-                <button className="dsmovie-pagination-button" disabled={isArrowRightDisabled} >
+                <p>{`${page.number+1} de ${page.totalPages}`}</p>
+                <button className="dsmovie-pagination-button" 
+                 disabled={page.last} onClick={() => onChange(page.number+1)}>
                     <Arrow className="dsmovie-flip-horizontal" />
                 </button>
             </div>
